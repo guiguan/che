b6e75f951c826cc77dadb702f4996ff73e7a65f0
Yevhenii Voevodin <evoevodin@codenvy.com>
2016-12-05 11:16:51 +0200

    Add license

--- exec-agent/src/rpc/model.go
+++ exec-agent/src/rpc/model.go
@@ -1,3 +1,14 @@
+//
+// Copyright (c) 2012-2016 Codenvy, S.A.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+//
+// Contributors:
+//   Codenvy, S.A. - initial API and implementation
+//
+
 // Provides a lightweight implementation of jsonrpc2.0 protocol.
 // The original jsonrpc2.0 specification - http://www.jsonrpc.org/specification
 //
